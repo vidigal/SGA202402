d468887259134a088565b12940f66e4c41ba2b66
VICTOR VIDIGAL RIBEIRO <victor.ribeiro1@GRANBERY.local>
2024-09-10 20:35:54 -0300

    incluindo campo imagem

--- sga/aplic/models.py
+++ sga/aplic/models.py
@@ -1,12 +1,20 @@
+import uuid
+
 from django.db import models
+from stdimage.models import StdImageField
 from django.utils.translation import gettext_lazy as _
+
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
 
 # Create your models here.
 class Curso(models.Model):
     nome = models.CharField(_('Nome'), max_length=100)
     descricao = models.TextField(_('Descrição'), max_length=500)
     carga_horaria = models.IntegerField(_('Carga Horária'))
-    #imagem = StdImageField(_('Imagem'), null=True, blank=True, upload_to=get_file_path, variations={'thumb': {'width': 420, 'height': 260, 'crop': True}})
+    imagem = StdImageField(_('Imagem'), null=True, blank=True, upload_to=get_file_path, variations={'thumb': {'width': 420, 'height': 260, 'crop': True}})
 
 
     class Meta:
