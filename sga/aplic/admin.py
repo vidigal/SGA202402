ac507b1b948f1694086a71028c84f9d0a2fbb6b3
VICTOR VIDIGAL RIBEIRO <victor.ribeiro1@GRANBERY.local>
2024-09-12 21:57:51 -0300

    admin completo

--- sga/aplic/admin.py
+++ sga/aplic/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 
-from .models import Curso
+from .models import Curso, Professor, Aluno, Disciplina, Turma
 
 
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'carga_horaria')
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'titulacao')
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'curso')
+
+@admin.register(Disciplina)
+class DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'carga_horaria')
+
+@admin.register(Turma)
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ('ano', 'semestre', 'disciplina')
+
