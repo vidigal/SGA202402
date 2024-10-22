6ea392d5c7a6a0eebaea2555a9eac2da596e57ee
VICTOR VIDIGAL RIBEIRO <victor.ribeiro1@GRANBERY.local>
2024-10-22 20:36:53 -0300

    área do usuario

--- sga/sga/settings.py
+++ sga/sga/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap5',
     'aplic',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
